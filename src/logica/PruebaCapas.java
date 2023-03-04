8349052229fc52722b379457e0acc0db4ea00f02
Luciano Cordoba <YassLuciano>
2023-03-04 12:02:36 -0300

    primer commit

---
+++ src/logica/PruebaCapas.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import igu.Pantalla;
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+      
+        Pantalla screen = new Pantalla();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+        
+    }
+    
+}
